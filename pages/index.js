afcb0d9b6db2402511b5b3694ee21cb53effefb3
Richard <rsrprivat@gmail.com>
2021-11-08 21:00:47 +0100

    ResourcePicker experimenting

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,39 @@
+import { useState } from "react";
 import { Heading, Page, EmptyState, Layout } from "@shopify/polaris";
+import createApp from "@shopify/app-bridge";
+import { ResourcePicker } from "@shopify/app-bridge-react";
 
-const Index = () => (
-  <Page>
-    <Layout>
-      <EmptyState
-        heading="Manage your inventory transfers"
-        action={{ content: "Select products" }}
-        secondaryAction={{
-          content: "Learn more",
-          url: "https://help.shopify.com",
-        }}
-        image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
-      >
-        <p>Select Products</p>
-      </EmptyState>
-    </Layout>
-  </Page>
-);
+const Index = () => {
+  const [modal, setModal] = useState({ open: false });
 
+  const handleClick = () => {
+    setModal({ open: !modal });
+  };
+
+  return (
+    <Page>
+      <ResourcePicker
+        resourceType="Product"
+        open={modal.open}
+        onCancel={handleClick}
+      />
+      <Layout>
+        <EmptyState
+          heading="Manage your inventory transfers"
+          action={{
+            content: "Select products",
+            onAction: () => setModal({ open: true }),
+          }}
+          secondaryAction={{
+            content: "Learn more",
+            url: "https://help.shopify.com",
+          }}
+          image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
+        >
+          <p>Select Products</p>
+        </EmptyState>
+      </Layout>
+    </Page>
+  );
+};
 export default Index;
