911dced74e35fbd384c260a740168088381380ab
Richard <rsrprivat@gmail.com>
2021-11-08 22:36:25 +0100

    some styling to display the products

--- pages/components/ProductList.js
+++ pages/components/ProductList.js
@@ -48,14 +48,46 @@ const ProductList = () => {
   console.log("data", data);
 
   return (
-    <div>
-      <h1>Selected Products in the localStorage:</h1>
-      <p>
-        {data.nodes.map((item) => {
-          return <div key={item.id}>{item.title}</div>;
-        })}
-      </p>
-    </div>
+    <Card>
+      <ResourceList
+        showHeader
+        resourceName={{ singular: "Product", plural: "Products" }}
+        items={data.nodes}
+        renderItem={(item) => {
+          const media = (
+            <Thumbnail
+              source={
+                item.images.edges[0]
+                  ? item.images.edges[0].node.originalSrc
+                  : ""
+              }
+              alt={
+                item.images.edges[0] ? item.images.edges[0].node.altText : ""
+              }
+            />
+          );
+          const price = item.variants.edges[0].node.price;
+          return (
+            <ResourceList.Item
+              id={item.id}
+              media={media}
+              accessibilityLabel={`View details for ${item.title}`}
+            >
+              <Stack>
+                <Stack.Item fill>
+                  <h3>
+                    <TextStyle variation="strong">{item.title}</TextStyle>
+                  </h3>
+                </Stack.Item>
+                <Stack.Item>
+                  <p>{price} SEK</p>
+                </Stack.Item>
+              </Stack>
+            </ResourceList.Item>
+          );
+        }}
+      />
+    </Card>
   );
 };
 
