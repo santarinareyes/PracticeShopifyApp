59e6e135c2fff0b9945412151f7bc1ff5d2220fa
Richard <rsrprivat@gmail.com>
2021-11-08 18:43:28 +0100

    start

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { Heading, Page } from "@shopify/polaris";
+
+const Index = () => (
+  <Page>
+    <Heading>Shopify app with Node and React by Richard🎉</Heading>
+  </Page>
+);
+
+export default Index;
